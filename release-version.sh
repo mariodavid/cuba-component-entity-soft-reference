ecffbeb68301f24c89dfb4e28d1c14a8599986dc
Mario <mario@die-davids.de>
2018-10-31 12:51:07 +0100

    renamed release script

--- release-version.sh
+++ release-version.sh
@@ -2,10 +2,10 @@
 
 if [[ -z "$1" ]]
   then
-    echo "Define tag version. Usage: ./tag-release.sh 0.3.1"
+    echo "Define tag version. Usage: ./release-version.sh 0.3.1"
 
   else
     git tag -a $1 -m "version $1"
     git push origin $1
-    git tag
+    echo "Release $1 has been created..."
 fi
